c59e84d514b396d4fbf6da72686bf09d0de8d5e1
angelchanquin <ajchanquin@gmail.com>
2018-02-27 01:52:23 -0600

    Added enums

--- src/main/java/com/angelchanquin/pharmacrm/domain/enumeration/RecibidoOrdenDeCompra.java
+++ src/main/java/com/angelchanquin/pharmacrm/domain/enumeration/RecibidoOrdenDeCompra.java
@@ -4,5 +4,5 @@ package com.angelchanquin.pharmacrm.domain.enumeration;
  * The RecibidoOrdenDeCompra enumeration.
  */
 public enum RecibidoOrdenDeCompra {
-    NO_RECIBIDO,  PARCIAL,  TOTAL
+    NO_RECIBIDO, PARCIAL, TOTAL
 }
